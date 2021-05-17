681fbc83e6d43c148a2bd7adc9adaf532333c4f0
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2021-05-17 19:18:52 +0200

    menuitem

--- content/zotero-prosec-linker.ts
+++ content/zotero-prosec-linker.ts
@@ -3,11 +3,20 @@ declare const Zotero: any
 
 const monkey_patch_marker = 'ProsecLinkerMonkeyPatched'
 
+type Item = {
+  isNote: () => boolean
+  isAnnotation?: () => boolean
+  isAttachment?: () => boolean
+}
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function patch(object, method, patcher) {
   if (object[method][monkey_patch_marker]) return
   object[method] = patcher(object[method])
   object[method][monkey_patch_marker] = true
+}
+
+function selected(): Item[] {
+  return (Zotero.getActiveZoteroPane().getSelectedItems() as Item[]).filter((item: Item) => !(item.isNote() || item.isAttachment() || item.isAnnotation?.()))
 }
 
 class ProsecLinker { // tslint:disable-line:variable-name
@@ -23,6 +32,13 @@ class ProsecLinker { // tslint:disable-line:variable-name
     this.initialized = true
 
     this.strings = globals.document.getElementById('zotero-prosec-linker-strings')
+
+    const self = this // eslint-disable-line @typescript-eslint/no-this-alias
+    patch(Zotero.getActiveZoteroPane(), 'buildItemContextMenu', original => async function ZoteroPane_buildItemContextMenu() {
+      await original.apply(this, arguments) // eslint-disable-line prefer-rest-params
+      const menuitem = self.globals.document.getElementById('prosec-link')
+      menuitem.hidden = selected().length === 0
+    })
   }
 
   openPreferenceWindow() {
@@ -31,6 +47,11 @@ class ProsecLinker { // tslint:disable-line:variable-name
       'prosec-linker-preferences',
       `chrome,titlebar,toolbar,centerscreen${Zotero.Prefs.get('browser.preferences.instantApply', true) ? 'dialog=no' : 'modal'}`
     )
+  }
+
+  link() {
+    const items = selected()
+    alert(`${items.length}`)
   }
 }
 
